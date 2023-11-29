2cd10927d07d0f015957f8bed52d4f40850115ec
ln <2044593394@qq.com>
2023-11-29 18:55:03 +0800

    隐官大人

--- src/main/java/love/forever/ny.java
+++ src/main/java/love/forever/ny.java
@@ -6,5 +6,6 @@ public class ny {
         System.out.println("南宫燕 女娲石");
         System.out.println("剑气长城 剑修 陆芝");
         System.out.println("剑气长城 剑修 董三更");
+        System.out.println("剑气长城 剑修 隐官大人");
     }
 }
