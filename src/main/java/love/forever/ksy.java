29b9ea156317693624b67213e2dfc7af06c8e620
ln <2044593394@qq.com>
2023-11-29 17:53:08 +0800

    阿良第一剑

---
+++ src/main/java/love/forever/ksy.java
@@ -0,0 +1,7 @@
+package love.forever;
+
+public class ksy {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/love/forever/ny.java
@@ -0,0 +1,7 @@
+package love.forever;
+
+public class ny {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
